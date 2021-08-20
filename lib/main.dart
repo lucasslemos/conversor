cde53d989b6fc1ab8a9e4b7c9e5c278f29ff64c0
lucasslemos <lucas.lemos.dev@gmail.com>
2021-08-20 12:08:37 -0400

    Requisitando os Dados

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json-cors&key=7b2aadc8";
+
+
+void main() async {
+
+  runApp(MaterialApp(
+      home: Home()
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body)["results"]["currencies"]["USD"];
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber
+          ,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Conversor de Moedas'),
+        ),
+      ),
+    );
+  }
+}
