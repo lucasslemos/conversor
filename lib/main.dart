944209a5cbf14565482885f8a0b927f15f243807
lucasslemos <lucas.lemos.dev@gmail.com>
2021-08-23 12:29:38 -0400

    Add TextField reais, dólares e euros

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,24 @@ const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=7b2aadc8";
 
 void main() async {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
+  ));
 }
 
-Future<Map> getData() async {
-  http.Response response = await http.get(request);
-  return json.decode(response.body)["results"]["currencies"]["USD"];
+Future<Map?> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -24,16 +36,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double? dolar;
+  double? euro;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
           backgroundColor: Colors.black,
-          appBar: AppBar(
-            title: Text("\$ Conversor \$"),
+          appBar:   AppBar(
+            title: Text("\$ Conversor \$", style: TextStyle(color: Colors.black),),
             backgroundColor: Colors.amber,
             centerTitle: true,
           ),
-      body: FutureBuilder<Map>(
+      body: FutureBuilder<Map?>(
           future: getData(),
           builder: (context, snapshot) {
             switch(snapshot.connectionState){
@@ -53,13 +70,62 @@ class _HomeState extends State<Home> {
                         style: TextStyle(
                             color: Colors.amber,
                             fontSize: 25.0),
-                        textAlign: TextAlign.center,)
+                        textAlign: TextAlign.center,
+                      ));
+                } else {
+                  dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 120.0, color: Colors.amber),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$"
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber, fontSize: 25.0
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Dólares",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "U\$"
+                          ),
+                          style: TextStyle(
+                              color: Colors.amber, fontSize: 25.0
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Euros",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "€\$"
+                          ),
+                          style: TextStyle(
+                              color: Colors.amber, fontSize: 25.0
+                          ),
+                        )
+                      ],
+                    ),
                   );
-                } else{
-                  return Container(color: Colors.green,);
                 }
             }
           })
     );
   }
 }
+
+
+
